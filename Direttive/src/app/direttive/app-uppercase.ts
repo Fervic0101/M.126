e3752045ab6093a15bb794484516944d769daf46
Marco <marcostufa00@gmail.com>
2025-09-04 12:02:55 +0200

    Es 3
    Es 3

--- Direttive/src/app/direttive/app-uppercase.ts
+++ Direttive/src/app/direttive/app-uppercase.ts
@@ -9,4 +9,15 @@ export class AppUppercase {
   @HostListener('input') onInput() {
     this.htmlElement.nativeElement.style.textTransform = 'uppercase';
   }
+
+  @HostListener('input') numberColor() {
+    if (this.htmlElement.nativeElement.id === 'number') {
+      const value = this.htmlElement.nativeElement.value;
+      if (value > 10) {
+        this.htmlElement.nativeElement.style.color = 'green';
+      } else {
+        this.htmlElement.nativeElement.style.color = 'red';
+      }
+    }
+  }
 }
