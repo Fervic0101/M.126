9b098846d82775b6c731204a1baed8a2563e09cb
Marco <marcostufa00@gmail.com>
2025-09-04 11:41:14 +0200

    Es 1
    Es 1

--- Direttive/src/app/direttive/mydirettive.ts
+++ Direttive/src/app/direttive/mydirettive.ts
@@ -1,19 +1,16 @@
 import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appMydirettive]'
+  selector: '[appMydirettive]',
 })
 export class Mydirettive {
   @Input() appMydirettive = '';
-  constructor(private htmlElement: ElementRef) 
-  { 
-    this.htmlElement.nativeElement.style.color = this.appMydirettive || 'yellow';
+  constructor(private htmlElement: ElementRef) {
+    this.htmlElement.nativeElement.style.color = this.appMydirettive || 'lime';
   }
   @HostListener('mouseenter') onMouseEnter() {
-    console.log(this.appMydirettive)
-    this.htmlElement.nativeElement.style.backgroundColor = this.appMydirettive || 'yellow';
-  }
-  @HostListener('mouseleave') onMouseLeave() {
-    this.htmlElement.nativeElement.style.backgroundColor = 'lightblue';
+    console.log(this.appMydirettive);
+    this.htmlElement.nativeElement.style.backgroundColor =
+      this.appMydirettive || 'blue';
   }
 }
