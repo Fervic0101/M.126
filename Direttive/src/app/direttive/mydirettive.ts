4afbc968f78f3e09bf10c9bd6f58740d4b675360
ANNA <anna.berti.14-1@accademiadigitaleliguria.it>
2025-09-04 12:16:41 +0200

    esercizio 3

--- Direttive/src/app/direttive/mydirettive.ts
+++ Direttive/src/app/direttive/mydirettive.ts
@@ -11,7 +11,7 @@ export class Mydirettive {
   }
   @HostListener('mouseenter') onMouseEnter() {
     console.log(this.appMydirettive)
-    this.htmlElement.nativeElement.style.backgroundColor = this.appMydirettive || 'blue';
+    this.htmlElement.nativeElement.style.backgroundColor = this.appMydirettive || 'white';
   }
   @HostListener('mouseleave') onMouseLeave() {
     this.htmlElement.nativeElement.style.backgroundColor = 'lightblue';
@@ -19,10 +19,16 @@ export class Mydirettive {
   //Crea una direttiva appHighlight che cambia colore di sfondo quando il mouse passa sopra. Usa @Input() per permettere di scegliere il colore.
   //Crea una direttiva appUppercase che trasforma tutto il testo di un input in maiuscolo mentre scrivi. Usa HostListener('input').
   @HostListener('input') onInputChange() {
-    const testo = this.htmlElement.nativeElement as HTMLElement;
+    const testo = this.htmlElement.nativeElement;
     if (testo instanceof HTMLInputElement) {
       testo.value = testo.value.toUpperCase();
     }
+    // per input numerici. Se il valore è > 10 → verde, < 10 → rosso. Usare HostListener('input') per controllare il valore e cambiare stile dinamicamente.
+    if (testo.type === 'number') {
+      const num = testo.value;
+      testo.style.color = num > 10 ? 'green' : 'red';
+      
+      }
 
   }
 }
