d4a77e1e41d5ba7db0fc78a341c549a1d5907bac
Fervic1991 <gallardo.oscar@rocketmail.com>
2025-09-04 10:53:04 +0200

    direttive modifiche

--- Direttive/src/app/app.ts
+++ Direttive/src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Mydirettive } from './direttive/mydirettive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Mydirettive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App  {
+  colore = "red"
   protected readonly title = signal('Direttive');
+  constructor() {
+
+  }
+
 }
---
+++ Direttive/src/app/direttive/mydirettive.spec.ts
@@ -0,0 +1,8 @@
+import { Mydirettive } from './mydirettive';
+
+describe('Mydirettive', () => {
+  it('should create an instance', () => {
+    const directive = new Mydirettive();
+    expect(directive).toBeTruthy();
+  });
+});
