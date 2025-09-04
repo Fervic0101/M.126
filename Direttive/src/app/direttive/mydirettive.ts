83580b5bc2fb2e6c46e15515dfb2819cb71bb365
ANNA <anna.berti.14-1@accademiadigitaleliguria.it>
2025-09-04 12:06:54 +0200

    esercizio 2

--- Direttive/src/app/direttive/mydirettive.ts
+++ Direttive/src/app/direttive/mydirettive.ts
@@ -11,9 +11,18 @@ export class Mydirettive {
   }
   @HostListener('mouseenter') onMouseEnter() {
     console.log(this.appMydirettive)
-    this.htmlElement.nativeElement.style.backgroundColor = this.appMydirettive || 'yellow';
+    this.htmlElement.nativeElement.style.backgroundColor = this.appMydirettive || 'blue';
   }
   @HostListener('mouseleave') onMouseLeave() {
     this.htmlElement.nativeElement.style.backgroundColor = 'lightblue';
+  }
+  //Crea una direttiva appHighlight che cambia colore di sfondo quando il mouse passa sopra. Usa @Input() per permettere di scegliere il colore.
+  //Crea una direttiva appUppercase che trasforma tutto il testo di un input in maiuscolo mentre scrivi. Usa HostListener('input').
+  @HostListener('input') onInputChange() {
+    const testo = this.htmlElement.nativeElement as HTMLElement;
+    if (testo instanceof HTMLInputElement) {
+      testo.value = testo.value.toUpperCase();
+    }
+
   }
 }
