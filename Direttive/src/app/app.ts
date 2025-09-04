37b69ffa57b77b7a149debe80609edd3cfd45136
user05_14_1 <user05_14_1@accademiadigitalegenova.onmicrosoft.com>
2025-09-04 12:08:18 +0200

    ES 2 (DUE) Benevelli

--- Direttive/src/app/app.ts
+++ Direttive/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Mydirettive } from './direttive/mydirettive';
+import { AppUppercase } from "./direttive/app-uppercase";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Mydirettive],
+  imports: [RouterOutlet, Mydirettive, AppUppercase],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Direttive/src/app/direttive/mydirettive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Mydirettive } from './mydirettive';
-
-describe('Mydirettive', () => {
-  it('should create an instance', () => {
-    const directive = new Mydirettive();
-    expect(directive).toBeTruthy();
-  });
-});
