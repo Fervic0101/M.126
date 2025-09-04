b29a13b69bf1ded7a9c8dc4ae770da0862ebe12a
ANNA <anna.berti.14-1@accademiadigitaleliguria.it>
2025-09-04 12:48:52 +0200

    esercizio 5. finito

--- Direttive/src/app/direttive/mydirettive.ts
+++ Direttive/src/app/direttive/mydirettive.ts
@@ -8,7 +8,7 @@ export class Mydirettive {
   clickCount = 0; //variabili vanno dichiarate prima del costruttore 
   constructor(private htmlElement: ElementRef) 
   { 
-    this.htmlElement.nativeElement.style.color = this.appMydirettive || 'yellow';
+    this.htmlElement.nativeElement.style.color = this.appMydirettive || 'black';
   }
   @HostListener('mouseenter') onMouseEnter() {
     console.log(this.appMydirettive)
@@ -28,15 +28,43 @@ export class Mydirettive {
     if (testo.type === 'number') {
       const num = testo.value;
       testo.style.color = num > 10 ? 'green' : 'red';
-      
-      }
+    }
+    //Crea una direttiva appPasswordStrength da applicare a un input password. Mostra dinamicamente un bordo verde/rosso secondo la forza della password:
+    //Debole < 6 caratteri. Media 6-10. Forte > 10 e contiene numeri + lettere
+
+    if (testo.type === 'password') {
+      const val = testo.value;
+      let strength = 'Debole';
+      let borderColor = 'red';
+
+      const hasLetters = /[a-zA-Z]/.test(val); //verifica se il valore val contiene almeno un carattere
+      const hasNumbers = /[0-9]/.test(val);
+
+      if (val.length > 10 && hasLetters && hasNumbers) {
+        strength = 'Forte';
+        borderColor = 'green';
+      } else if (val.length >= 6) {
+        strength = 'Media';
+        borderColor = 'orange';
+      } //di default lo avevo messo rosso
+
+      testo.style.border = `4px solid ${borderColor}`;
+
+      //Loggare anche un messaggio in console (“Debole”, “Media”, “Forte”).
+      console.log(strength);
+    }
   }
-  //Conta quante volte un elemento è stato cliccato e logga in console il numero di click. Usare HostListener('click').
+
+    //Conta quante volte un elemento è stato cliccato e logga in console il numero di click. Usare HostListener('click').
   @HostListener('click')  onClick(){
     this.clickCount++;
     console.log(`Elemento cliccato ${this.clickCount} volte`);
   }
+  }
+  
 
 
 
-}
+
+
+
