a24ec57eb37050cc25516999c63a2bb66e33b4c4
Marco <marcostufa00@gmail.com>
2025-09-04 12:08:40 +0200

    Es 4
    Es 4

--- Direttive/src/app/direttive/mydirettive.ts
+++ Direttive/src/app/direttive/mydirettive.ts
@@ -5,6 +5,7 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class Mydirettive {
   @Input() appMydirettive = '';
+  private clickCount = 0;
   constructor(private htmlElement: ElementRef) {
     this.htmlElement.nativeElement.style.color = this.appMydirettive || 'lime';
   }
@@ -12,5 +13,9 @@ export class Mydirettive {
     console.log(this.appMydirettive);
     this.htmlElement.nativeElement.style.backgroundColor =
       this.appMydirettive || 'blue';
+  }
+  @HostListener('click') onClick() {
+    this.clickCount++;
+    console.log(this.clickCount);
   }
 }
