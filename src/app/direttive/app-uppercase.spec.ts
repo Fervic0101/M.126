75b30f09c2c72230ceb0ab556f80e3bd6548788f
casta-yr <matteo.castagnola.14-1@accademiadigitaleliguria.it>
2025-09-05 11:06:45 +0200

    Delete src directory

--- src/app/direttive/app-uppercase.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppUppercase } from './app-uppercase';
-
-describe('AppUppercase', () => {
-  it('should create an instance', () => {
-    const directive = new AppUppercase();
-    expect(directive).toBeTruthy();
-  });
-});
