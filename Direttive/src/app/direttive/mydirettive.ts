1a5c0bc4280743de5ce0cccbbfd4701490201439
Nahol23 <nouhailamaarouf2003@gmail.com>
2025-09-05 10:19:44 +0200

    changes

--- Direttive/src/app/direttive/mydirettive.ts
+++ Direttive/src/app/direttive/mydirettive.ts
@@ -5,15 +5,42 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class Mydirettive {
   @Input() appMydirettive = '';
-  constructor(private htmlElement: ElementRef) 
-  { 
+
+  constructor(private htmlElement: ElementRef<HTMLInputElement>) { 
+ 
     this.htmlElement.nativeElement.style.color = this.appMydirettive || 'yellow';
+   
   }
+
   @HostListener('mouseenter') onMouseEnter() {
-    console.log(this.appMydirettive)
+    console.log(this.appMydirettive);
     this.htmlElement.nativeElement.style.backgroundColor = this.appMydirettive || 'yellow';
   }
+
   @HostListener('mouseleave') onMouseLeave() {
     this.htmlElement.nativeElement.style.backgroundColor = 'lightblue';
+  }
+
+  @HostListener('input', ['$event']) onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+  
+    input.value = input.value.toUpperCase();
+
+   
+    const value = Number(input.value);
+    if (!isNaN(value)) {
+      if (value > 10) {
+        input.style.color = 'green';
+      } else if (value < 10) {
+        input.style.color = 'red';
+      } else {
+        input.style.color = 'black'; 
+      }
+    }
+  }
+
+  private highlight(color: string) {
+    this.htmlElement.nativeElement.style.backgroundColor = color;
   }
 }
