8c44c4d5f54b5167db04872fa13dacf014a282bf
Nahol23 <nouhailamaarouf2003@gmail.com>
2025-09-08 09:19:08 +0200

    changes

--- Dashboard/src/app/prodotti/prodotti.ts
+++ Dashboard/src/app/prodotti/prodotti.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { ProdottiModel } from '../Model/ProdottiModel';
+import { CommonModule } from '@angular/common';  
 
 @Component({
   selector: 'app-prodotti',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './prodotti.html',
   styleUrl: './prodotti.css'
 })
@@ -15,9 +16,9 @@ export class Prodotti {
   Carrefour : Array<ProdottiModel> = [];;
   constructor()
   {
-    this.Coop.push(new ProdottiModel('Puma',75,'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Puma_AG.svg/2560px-Puma_AG.svg.png','Scarpe da ginnastica di alta qualità',true));
-    this.Esselunga.push(new ProdottiModel('Nike',100,'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2c5f1f3e-1d2e-4f6a-8b1e-3b4c6f4d5e0c/nike-just-do-it.jpg','Scarpe da ginnastica di alta qualità',true));
-    this.Carrefour.push( new ProdottiModel('Adidas',50,'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/2560px-Adidas_Logo.svg.png','Scarpe da ginnastica di alta qualità',false));
+    this.Coop.push(new ProdottiModel('Puma',75,'https://imgs.search.brave.com/B287woB3ugESSzHuDAx0D-_2ixt17bp6Ei8XCtdEEVU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuc3RpbGVvLml0/L2ltZy8yMjh4Mjk4/YmUvNTIxMDQwNzQ3/LXNuZWFrZXJzLXB1/bWEuanBn','Scarpe di alta qualità',true));
+    this.Esselunga.push(new ProdottiModel('Nike',100,'https://imgs.search.brave.com/wwLSRonHMqWRGSHVaOHnR-o6C9PMF1lJmfEZcIRzAgc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pOC5h/bXBsaWVuY2UubmV0/L3QvanBsL2pkaXRf/cHJvZHVjdF9saXN0/P3BsdT1qZF80NTcw/NjdfYWwmcWx0PTky/Jnc9MzYzJmg9MzYz/JnY9MSZmbXQ9YXV0/bw','Scarpe  di alta qualità',true));
+    this.Carrefour.push( new ProdottiModel('Adidas',50,'https://imgs.search.brave.com/4U_RefmLxNwtb1qZJQBVCqgl9Lb-zlgSnA-MluhtLJk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zY2Fy/cGFtb25kby5pdC9k/dy9pbWFnZS92Mi9C/SERGX1BSRC9vbi9k/ZW1hbmR3YXJlLnN0/YXRpYy8tL1NpdGVz/LVNDQVJQQU1PTkRP/LU1BU1RFUi9kZWZh/dWx0L2R3N2JhMzdj/YmIvaW1hZ2VzL2xh/cmdlL1NDNTM1MzIw/LTAwMDEuanBnP3N3/PTQwMCZzaD00MDA','Scarpe  di alta qualità',false));
   }
   ngOnInit(){
     switch(this.supermarket)
